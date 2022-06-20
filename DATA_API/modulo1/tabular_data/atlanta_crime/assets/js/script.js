ececc80a2139bdad092aa1d0f979d0443dd4f900
Bernardo Alves <bernardoalves330@gmail.com>
2022-06-20 17:03:14 -0300

    added csv parsing

--- DATA_API/modulo1/tabular_data/atlanta_crime/assets/js/script.js
+++ DATA_API/modulo1/tabular_data/atlanta_crime/assets/js/script.js
@@ -1,14 +1,18 @@
 async function getCrimeData(){
     const response = await fetch('assets/csv/atlcrime.csv');
-    const tab = await response.text();
-    const data = $.csv.toArray(tab);
-
-    console.log(data);
+    if(response.ok){
+        const text = await response.text();
+        const table = await text.split('\n').splice(1);
+        table.forEach(row =>{
+            const line = row.split(',');
+            const crime = line[1];
+            const dateString = line[3]
+            const dateArray = dateString.split('/');
+            console.log(dateArray);
+        })
+    }else{
+        console.log('NÃO')
+    }
 }
 
 getCrimeData();
-
-// $.get('assets/csv/atlcrime.csv', function(CSVdata){
-//     data = $.csv.toArray(CSVdata);
-//     console.log(data);
-// })
