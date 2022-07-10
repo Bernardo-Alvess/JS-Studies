499fdcc1fe4b85c4eba336e28e8b619577bae458
Bernardo Alves <bernardoalves330@gmail.com>
2022-07-10 19:39:00 -0300

    i forgor

--- DATA_API/modulo2/index.js
+++ DATA_API/modulo2/index.js
@@ -1,6 +1,16 @@
+const { json } = require('express');
 const express = require('express');
 const app  = express();
-
 app.listen(3000)
-
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+app.post('/api', (request, response) => {
+    console.log("I got a request");
+    console.log(request.body)
+    response.json({
+        status: 'success',
+        latitude: request.body.lat,
+        longitude: request.body.lon
+    });
+})
--- DATA_API/modulo2/public/script.js
+++ DATA_API/modulo2/public/script.js
@@ -1,15 +1,42 @@
+function createMap(coords, zoom){
+    let map = L.map('map').setView(coords, zoom);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    let marker = L.marker(coords);
+
+    marker.bindPopup("Your location");
+    marker.addTo(map);
+}
+
 if('geolocation' in navigator){
-console.log("Geolocation is available");
+    console.log("Geolocation is available");
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        document.getElementById('latitude').textContent = lat;
+        document.getElementById('longitude').textContent = lon;
 
-navigator.geolocation.getCurrentPosition(function (position) {
+        const data = {lat, lon};
+        console.log(data)
+        const options = {
+            method: 'POST',
+            Headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
 
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+        fetch('/api', options).then(response =>{
+            console.log(response.json())
+        })
 
-    document.getElementById('latitude').textContent = lat;
-    document.getElementById('longitude').textContent = lon;
-})
+        createMap([lat, lon], 13);
+    })
 
+    
 }else{
     console.log("Geolocation is NOT available");
 }
+
